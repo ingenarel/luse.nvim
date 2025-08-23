4f914d9a2fc9e7d30830b62c6ddd35a94c8eba5d
ingenarel <ingenarelitems@gmail.com>
2025-08-23 16:20:04 +0600

    (misc): format

--- lua/luse/init.lua
+++ lua/luse/init.lua
@@ -1,33 +1,33 @@
 local m = {}
 
 function m.packageNameToUseFlags(package)
-	local commands = {
-		["bash-language-server"] = function()
-			return "\n#bash-language-server\nnet-libs/nodejs npm\n"
-		end,
-		["yaml-language-server"] = function()
-			return "\n#yaml-language-server\nnet-libs/nodejs npm\n"
-		end,
-		["clangd"] = function()
-			return "\n#clangd\nllvm-core/clang extra"
-		end,
-		["clang-tidy"] = function()
-			return "\n#clang-tidy\nllvm-core/clang extra"
-		end,
-		default = function()
-			return ""
-		end,
-	}
-	local fn = commands[package] or commands.default
-	return fn()
+    local commands = {
+        ["bash-language-server"] = function()
+            return "\n#bash-language-server\nnet-libs/nodejs npm\n"
+        end,
+        ["yaml-language-server"] = function()
+            return "\n#yaml-language-server\nnet-libs/nodejs npm\n"
+        end,
+        ["clangd"] = function()
+            return "\n#clangd\nllvm-core/clang extra"
+        end,
+        ["clang-tidy"] = function()
+            return "\n#clang-tidy\nllvm-core/clang extra"
+        end,
+        default = function()
+            return ""
+        end,
+    }
+    local fn = commands[package] or commands.default
+    return fn()
 end
 
 function m.generateFile(packages)
-	local data = ""
-	for i = 1, #packages do
-		data = data .. m.packageNameToUseFlags(packages[i])
-	end
-	return data
+    local data = ""
+    for i = 1, #packages do
+        data = data .. m.packageNameToUseFlags(packages[i])
+    end
+    return data
 end
 
 return m
